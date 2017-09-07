c5b554f0c39d0027df3c183bb9c6436ad2b8f057
Harrison Smith <s3658817@student.rmit.edu.au>
2017-09-07 18:07:16 +1000

    input string output morse code sounds

--- src/Tone.java
+++ src/Tone.java
@@ -21,11 +21,21 @@ import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Tone {
-//Tone generated from code sourced from; https://community.oracle.com/thread/1273219?start=0&tstart=0
-	public static void generateTone(int hz, int msecs, int volume) throws LineUnavailableException {
 
+	static char[] alphabetAndNumbers = {'a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')'};
+
+	static String[] morseCode = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+			"-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
+			"..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", ")" };
+
+	// Tone generated from code sourced from;
+	// https://community.oracle.com/thread/1273219?start=0&tstart=0
+	public static void generateTone(int hz, int msecs, int volume) throws LineUnavailableException {
 		float frequency = 44100;
 		byte[] buf;
 		AudioFormat af;
@@ -50,19 +60,51 @@ public class Tone {
 		sdl.stop();
 		sdl.close();
 	}
-	static int oneunit = 50;
-	public static void dot() throws LineUnavailableException{
+
+	static int oneunit = 70;
+
+	public static void dot() throws LineUnavailableException {
 		generateTone(590, oneunit, (int) (50 * 1.28));
 	}
-	public static void dash() throws LineUnavailableException{
-		generateTone(590, oneunit*3, (int) (50 * 1.28));
+
+	public static void dash() throws LineUnavailableException {
+		generateTone(590, oneunit * 3, (int) (50 * 1.28));
 	}
-	public static void main(String[] args) throws LineUnavailableException {
+
+	public static void SoundOutput(char c) throws LineUnavailableException {
+			for (int i = 0; i < alphabetAndNumbers.length;i++){
+				if (alphabetAndNumbers[i] == (Character.toLowerCase(c))){
+					morsetoSound(morseCode[i]);
+				}
+			}
+	}
+
+	public static void morsetoSound(String input) throws LineUnavailableException {
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == '.'){
+				dot();
+			}else{
+				dash();
+			}
 		
-		dot();
-		dot();
-		dash();
-		dot();
-		
+		}
+	}
+
+	public static void main(String[] args) throws LineUnavailableException, InterruptedException {
+
+		ArrayList<Character> test = new ArrayList<Character>();
+		Scanner aa = new Scanner(System.in);
+		String str = aa.nextLine();
+		for (int i = 0; i < str.length(); i++) {
+			test.add(str.charAt(i));
+			
+			if (str.charAt(i) == ' '){
+				System.out.println(System.getProperty("line.separator"));
+				Thread.sleep(2000);
+			}
+			
+			SoundOutput(str.charAt(i));
+		}
+		aa.close();
 	}
 }
