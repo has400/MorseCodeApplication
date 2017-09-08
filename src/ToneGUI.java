9ebd5c7cebecc4bcd031bbbeafe12446d09d2121
rmit-s3658817-harrison-smith <s3658817@student.rmit.edu.au>
2017-09-09 02:58:48 +1000

    gui

---
+++ src/ToneGUI.java
@@ -0,0 +1,59 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class ToneGUI {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ToneGUI window = new ToneGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ToneGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 187);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 11, 414, 41);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(10, 97, 414, 41);
+		frame.getContentPane().add(textField_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(177, 63, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
